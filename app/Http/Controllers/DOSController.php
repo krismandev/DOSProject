8b661725b2c2edcdd6384981ec45d750a1146897
krismandev <krismanpratama@gmail.com>
2021-12-03 16:06:56 +0700

    report dos

--- app/Http/Controllers/DOSController.php
+++ app/Http/Controllers/DOSController.php
@@ -10,6 +10,6 @@ class DOSController extends Controller
     public function reportDOS()
     {
         $dos = Dos::orderBy("created_at","desc")->get();
-        return view("dashboard.dos.reportDOS",compact(["dos"]));
+        return view("dashboard.dos.reportDos",compact(["dos"]));
     }
 }
