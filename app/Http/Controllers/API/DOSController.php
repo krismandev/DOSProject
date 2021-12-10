99407cdeb5fb7765f92abaa72cdd8b5db4684dfb
krismandev <krismanpratama@gmail.com>
2021-12-10 21:32:06 +0700

    perbaikan api

--- app/Http/Controllers/API/DOSController.php
+++ app/Http/Controllers/API/DOSController.php
@@ -89,7 +89,7 @@ class DOSController extends Controller
 
             foreach ($dos as $item) {
                 $item->user = User::find($item->user_id);
-                $item->foto = env("APP_URL").Storage::url($item->foto);
+                $item->foto = assert($item->foto);
                 $item->user->sf_data = SalesForce::where("user_id",$item->user_id)->first();
                 unset($item->user_id);
             }
