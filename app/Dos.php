0e77061d28c0da5a4aeaa1fe45cebbebaf6cc41a
krismandev <krismanpratama@gmail.com>
2022-05-04 20:42:15 +0700

    perbaikan api lapor dos

--- app/Dos.php
+++ app/Dos.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dos extends Model
 {
-    protected $fillable = ["user_id","foto","kegiatan","tanggal","waktu","product","long","lat","odp","produk","status_kunjungan","keterangan_kunjungan","keterangan_tambahan","status","id_dos"];
+    protected $fillable = ["user_id","foto","kegiatan","tanggal","waktu","product","long","lat","odp","produk","status_kunjungan","keterangan_kunjungan","keterangan_tambahan","status","id_dos","gab_kelurahan"];
 
     public function user()
     {
